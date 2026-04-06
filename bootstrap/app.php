466efc254074118353cfefb932ed10a1287837ca
dobro <dobrobut88@gmail.com>
2026-04-06 22:13:25 +0300

    fix: exclude supplier routes from CSRF verification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            '/supplier/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (NotFoundHttpException $e) {
